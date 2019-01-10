6a23b5f8e9ea115d9451fd2cd4bb21bfa4050eff
HanBitPix <hanjt517@gmail.com>
2019-01-09 18:43:54 -0800

    Fixed another bug on payment with Stripe

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,18 +4,16 @@ import { Link } from 'react-router-dom';
 import Payments from './Payments';
 
 class Header extends Component {
-  renderContent(){
-    switch(this.props.auth){
+  renderContent() {
+    switch (this.props.auth) {
       case null:
         return;
       case false:
-        return (
-          <li><a href="/auth/google">Login with Google</a></li>
-        );
+        return <li><a href="/auth/google">Login With Google</a></li>;
       default:
         return [
           <li key="1"><Payments /></li>,
-          <li key="3" style={{margin: '0 10px'}}>
+          <li key="3" style={{ margin: '0 10px' }}>
             Credits: {this.props.auth.credits}
           </li>,
           <li key="2"><a href="/api/logout">Logout</a></li>
@@ -23,12 +21,12 @@ class Header extends Component {
     }
   }
 
-  render(){
+  render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <Link 
-            to={this.props.auth ? '/surveys' : '/'} 
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
           >
             Emaily
@@ -42,7 +40,7 @@ class Header extends Component {
   }
 }
 
-function mapStateToProps({auth}){
+function mapStateToProps({ auth }) {
   return { auth };
 }
 
